98cde30f8a70c4290904532a38a65c1fc66d2316
Trien Vo <s3907397@rmit.edu.vn>
2023-08-04 14:07:51 +0700

    Initial Commit

---
+++ TestGithub/TestGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGithubApp.swift
+//  TestGithub
+//
+//  Created by Trien Vo Hong on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
